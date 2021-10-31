084e3425c3793023bfab4c2aea5961c325550965
Humberto Pinheiro <humbhenri@gmail.com>
2021-10-31 20:46:18 -0300

    fix search by country

--- assets/js/country.js
+++ assets/js/country.js
@@ -38,12 +38,28 @@
         }
     });
 
-    // await populateChart('brazil', new Date(2021, 10, 1), new Date(2020, 1, 1), 'Deaths', 'Número de óbitos');
+    await populateChart('brazil', new Date(2021, 10, 1), new Date(2020, 1, 1), 'Deaths', 'Número de óbitos');
+
+    function dailyNumbers(data) {
+        const result = [];
+        result.push(0);
+        for (let i = 1; i < data.length; i++) {
+            result.push(data[i] - data[i-1]);
+        }
+        console.log(data)
+        console.log(result)
+        return result;
+    }
 
     async function populateChart(country, to, from, dados, label) {
-        const dataByCountry = await searchCountry(country, to, from);
+        console.log(JSON.stringify({country, to, from, dados, label}));
+        const dataByCountry = await searchCountry(country, from, to);
         const ctx = document.querySelector("#linhas").getContext("2d");
         if (!linesChart) {
+            const data = dailyNumbers(dataByCountry.map(d => d[dados]));
+            const mean = _.mean(data);
+            const meanArray = new Array(data.length);
+            _.fill(meanArray, mean);
             linesChart = new Chart(ctx, {
                 type: "line",
                 options: {
@@ -61,15 +77,27 @@
                     datasets: [
                         {
                             label: label,
-                            data: dataByCountry.map(d => d[dados]),
+                            data: data,
+                        },
+                        {
+                            label: 'Média',
+                            data: meanArray,
+                            borderColor: 'rgb(75, 192, 192)',
+                            fill: false,
+                            tension: 0.1,
                         }
                     ],
                 }
             });
         } else {
+            const data = dailyNumbers(dataByCountry.map(d => d[dados]));
+            const mean = _.mean(data);
+            const meanArray = new Array(data.length);
+            _.fill(meanArray, mean);
             linesChart.data.labels = dataByCountry.map(d => d.Date);
             linesChart.data.datasets[0].label = label;
-            linesChart.data.datasets[0].data = dataByCountry.map(d => d[dados]);
+            linesChart.data.datasets[0].data = data;
+            linesChart.data.datasets[1].data = meanArray;
             linesChart.update();
         }
     }
@@ -85,9 +113,20 @@
         }
     }
 
+    function formatDate(date) {
+        let [y, m, d] = date.split("-"),
+            data = new Date(y, +m-1, d),
+            dia  = data.getDate().toString(),
+            diaF = (dia.length == 1) ? '0'+dia : dia,
+            mes  = (data.getMonth()+1).toString(), //+1 pois no getMonth Janeiro começa com zero.
+            mesF = (mes.length == 1) ? '0'+mes : mes,
+            anoF = data.getFullYear();
+        return anoF + "-" + mesF + "-" + diaF;
+    }
+
     async function searchCountry(country, from, to) {
         try {
-            return (await axios.get(`${countryByStatus}/${country}?from=${from}&to=${to}`)).data;
+            return (await axios.get(`${countryByStatus}/${country}?from=${formatDate(from)}&to=${formatDate(to)}`)).data;
         } catch (e) {
             console.error(e);
         }
